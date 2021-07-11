d1cfa50960c36078f3856e2507b8878ed4c13f48
Engin Tekin <tekin.engin@yahoo.com>
2021-07-11 00:24:29 -0700

    added forward pass and pred_trans

--- YOLOv3/yolov3.py
+++ YOLOv3/yolov3.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+from util import *
+
 
 class EmptyLayer(nn.Module):
     def __init__(self):
@@ -135,3 +137,68 @@ def create_modules(blocks):
         output_filters.append(filters)
         
     return net_info, module_list
+
+
+class YOLOv3(nn.Module):
+    def __init__(self, cfgfile):
+        super(YOLOv3, self).__init__()
+        self.blocks = parse_cfg(cfgfile)
+        self.net_info, self.module_list = create_modules(self.blocks)
+        
+        
+    def forward(self, x, isCUDA):
+        modules = self.blocks[1:]
+        outputs = {}
+        
+        write = 0
+        for i, module in enumerate(modules):
+            
+            module_type = module["type"]
+            
+            if module_type in {"convolutional", "upsample"}:
+                x = self.module_list[i](x)
+                
+            elif module_type == "route":
+                
+                layers = module["layers"]
+                layers = [int(l) for l in layers]
+                
+                if (layers[0]) > 0:
+                    layers[0] = layers[0] - i
+
+                if len(layers) == 1:
+                    x = outputs[i + (layers[0])]
+                
+                else:
+                    if (layers[1]) > 0:
+                        layers[1] = layers[1] - i
+                    
+                    map1 = outputs[i + layers[0]]
+                    map2 = outputs[i + layers[1]]
+                    x = torch.cat((map1, map2), 1)
+                
+            elif module_type == "shortcut":
+                from_ = int(module["from"])
+                x = outputs[i-1] + outputs[i+from_]
+                
+            elif module_type == "yolo":
+                anchors = self.module_list[i][0].anchors
+                
+                inp_dim = int(self.net_info["height"])
+                num_classes = int(module["classes"])
+                
+                x = x.data
+                x = predict_transform(x, inp_dim, anchors, num_classes, isCUDA)
+                
+                if not write:
+                    detections = x
+                    write = 1
+                
+                else:
+                    detections = torch.cat((detections, x), 1)
+                
+                
+            outputs[i] = x
+            
+        return detections
+        
